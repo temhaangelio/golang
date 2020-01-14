8bc8d9511f7f42d4fb3f25b6ddb6f9c106be5eb6
temhaangelio <temhaangelio@gmail.com>
2020-01-14 15:43:00 +0300

    lesson 1

---
+++ 01-variable/01-variable.go
@@ -0,0 +1,21 @@
+//lesson 1
+//variable
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "Ahmet Yılmaz"
+	city := "Bursa"
+	var (
+		age  = 36
+		code = 16160
+	)
+
+	fmt.Println(name)
+	fmt.Println(city)
+	fmt.Println(age, code)
+}
